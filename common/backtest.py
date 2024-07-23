259f26c000fd0fbfb4c0ca392318d5cc21657b12
jaepil-choi <chljeffreyz@gmail.com>
2024-07-24 06:40:10 +0900

    Implement quantstats stats/plots to Backtest

--- common/backtest.py
+++ common/backtest.py
@@ -4,6 +4,8 @@ import numpy as np
 import gaps_config as gcfg
 import validator
 
+import quantstats as qs
+
 class Backtest:
     def __init__(self, 
                  alpha,
@@ -24,6 +26,7 @@ class Backtest:
 
         self._reindex_data()
         self._calculate()
+        self._setup_quantstats()
 
     def _reindex_data(self):
         self.returns_df = self.returns_df.reindex(
@@ -34,6 +37,10 @@ class Backtest:
     def _calculate(self):
         self.port_return = self.returns_df.mul(self.alpha_df.shift(1)).sum(axis=1)
         self.cum_return = (1 + self.port_return).cumprod() - 1
+    
+    def _setup_quantstats(self):
+        self.qs_stats = [stat for stat in dir(qs.stats) if stat[0] != '_']
+        self.qs_plots = [plot for plot in dir(qs.plots) if plot[0] != '_']
 
     def get_port_return(self):
         return self.port_return
@@ -43,6 +50,28 @@ class Backtest:
     
     def plot_cum_return(self):
         self.cum_return.plot()
+
+    ## quantstats
+
+    def show_available_stats(self):
+        return self.qs_stats
+    
+    def get_stat(self, stat_name):
+        assert stat_name in self.qs_stats, f"Invalid stat name. Available stats are: {self.qs_stats}"
+        
+        stat_func = getattr(qs.stats, stat_name)
+
+        return stat_func(self.port_return)
+
+    def show_available_plots(self):
+        return self.qs_plots
+
+    def get_plot(self, plot_name):
+        assert plot_name in self.qs_plots, f"Invalid plot name. Available plots are: {self.qs_plots}"
+        
+        plot_func = getattr(qs.reports._plots, plot_name)
+
+        return plot_func(self.port_return, show=False)
     
 
     
